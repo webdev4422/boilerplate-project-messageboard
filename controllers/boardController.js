4f9cd066fd9618fcbde2b9387098d1b8602ddac3
user <user@gmail.com>
2023-05-07 13:21:28 +0300

    WIP boards

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,19 +1,31 @@
 const Board = require('../models/boardModel.js')
+const Thread = require('../models/threadModel.js')
 
 // post: /api/threads/:board board=boardX; text=test; delete_password=aaaa
 const createBoard = async (req, res) => {
+  const objExist = await Board.find({ name: req.body.board })
+  if (objExist == 'empty') {
+    console.log(await Board.find({ name: req.body.board }))
+    console.log(objExist)
+    res.redirect(303, `/b/${req.body.board}/`) // 303 parameter to make redirect work
+    return
+  }
+  const thread = await Thread.create({
+    text: req.body.text,
+    password: req.body.delete_password,
+    created_on: Date.now(),
+    bumped_on: Date.now(),
+    replies: [],
+    replycount: 0,
+  })
   const board = await Board.create({
     name: req.body.board,
-    threads: [],
+    threads: [thread],
   })
-  console.log('ok')
-  console.log(board)
-  board.updateOne({ _id: this._id }, { $push: { threads: { ok: 'ok' } } })
-  console.log('OK')
   console.log(board)
   res.json(board)
   // Redirect to get /b/:board
-  // res.redirect(303, `/b/${req.body.board}/`) // 303 parameter to make redirect work
+  res.redirect(303, `/b/${req.body.board}/`) // 303 parameter to make redirect work
 }
 
 // get: /api/threads/:board
