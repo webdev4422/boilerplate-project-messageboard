57b9170b84d0e8a7f6febeca060292841fd4266a
user <user@gmail.com>
2023-05-07 12:03:38 +0300

    Add get /api/threads/:board response

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,7 +1,6 @@
 const Message = require('../models/message.js')
 
-// post: /api/threads/:board text=test; delete_password=aaaa
-// Response: // [{"_id":"6456b218ad743174db9b6dd0","text":"testXXX","created_on":"2023-05-06T20:01:28.805Z","bumped_on":"2023-05-06T20:01:28.805Z","replies":[],"replycount":0}]
+// post: /api/threads/:board board=boardX; text=test; delete_password=aaaa
 const postMessage = async (req, res) => {
   const message = await Message.create({
     text: req.body.text,
@@ -11,11 +10,17 @@ const postMessage = async (req, res) => {
     replies: [],
     replycount: 0,
   })
-  // res.json(message)
-  // console.log(req.body.board)
+  // Redirect to get /b/:board
   res.redirect(303, `/b/${req.body.board}/`) // 303 parameter to make redirect work
 }
-module.exports = postMessage
+
+// get: /api/threads/:board
+// Response: // [{"_id":"6456b218ad743174db9b6dd0","text":"testXXX","created_on":"2023-05-06T20:01:28.805Z","bumped_on":"2023-05-06T20:01:28.805Z","replies":[],"replycount":0}]
+const getMessages = async (req, res) => {
+  const message = await Message.find({})
+  res.json(message)
+}
+module.exports = { postMessage, getMessages }
 // app.route('/api/threads/general').post((app) => {
 // const messagesCollection = await Message.find()
 // console.log(messagesCollection)
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
-const postMessage = require('../controllers/messageController.js')
+const { postMessage, getMessages } = require('../controllers/messageController.js')
 
 module.exports = async function (app) {
   app.route('/api/threads/:board').post(postMessage)
-  // app.route('/api/threads/:board').get(postMessage)
+  app.route('/api/threads/:board').get(getMessages)
 
   app.route('/api/replies/:board')
 }
