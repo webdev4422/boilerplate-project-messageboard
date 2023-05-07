1c3a60d30702d21e896eb7e53390da481bf725c7
user <user@gmail.com>
2023-05-07 19:49:19 +0300

    WIP boards

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,15 +1,10 @@
 const Board = require('../models/boardModel.js')
 const Thread = require('../models/threadModel.js')
+const { createThread, viewThread } = require('../controllers/threadController.js')
 
 // post: /api/threads/:board board=boardX; text=test; delete_password=aaaa
 const createBoard = async (req, res) => {
-  const objExist = await Board.find({ name: req.body.board })
-  if (objExist == 'empty') {
-    console.log(await Board.find({ name: req.body.board }))
-    console.log(objExist)
-    res.redirect(303, `/b/${req.body.board}/`) // 303 parameter to make redirect work
-    return
-  }
+  // Create thread
   const thread = await Thread.create({
     text: req.body.text,
     password: req.body.delete_password,
@@ -18,12 +13,20 @@ const createBoard = async (req, res) => {
     replies: [],
     replycount: 0,
   })
+
+  // Check if board exist
+  const boardX = await Board.find({ name: req.body.board }) // return array
+  if (boardX.length > 0) {
+    boardX[0].threads.push(thread) // push thread onto board
+    // Redirect to get /b/:board
+    res.redirect(303, `/b/${req.body.board}/`) // 303 parameter to make redirect work
+    return
+  }
+  // Create board and push thread
   const board = await Board.create({
     name: req.body.board,
     threads: [thread],
   })
-  console.log(board)
-  res.json(board)
   // Redirect to get /b/:board
   res.redirect(303, `/b/${req.body.board}/`) // 303 parameter to make redirect work
 }
@@ -31,7 +34,7 @@ const createBoard = async (req, res) => {
 // get: /api/threads/:board
 // Response: // [{"_id":"6456b218ad743174db9b6dd0","text":"testXXX","created_on":"2023-05-06T20:01:28.805Z","bumped_on":"2023-05-06T20:01:28.805Z","replies":[],"replycount":0}]
 const viewBoard = async (req, res) => {
-  const board = await Board.find({})
+  const board = await Board.find({ name: req.body.board })
   res.json(board)
 }
 module.exports = { createBoard, viewBoard }
