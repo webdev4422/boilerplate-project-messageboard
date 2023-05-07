a21496c8af7281369cb33a5372df4356a1d2e9df
user <user@gmail.com>
2023-05-07 09:38:59 +0300

    Convert messageController to async syntax

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,8 +2,8 @@ const Message = require('../models/message.js')
 
 // post: /api/threads/general text=test; delete_password=aaaa
 // Response: // [{"_id":"6456b218ad743174db9b6dd0","text":"testXXX","created_on":"2023-05-06T20:01:28.805Z","bumped_on":"2023-05-06T20:01:28.805Z","replies":[],"replycount":0}]
-const postMessage = (req, res) => {
-  const messageX = new Message({
+const postMessage = async (req, res) => {
+  const message = await Message.create({
     text: req.body.text,
     password: req.body.delete_password,
     created_on: Date.now(),
@@ -11,8 +11,8 @@ const postMessage = (req, res) => {
     replies: [],
     replycount: 0,
   })
-    .save()
-    .then((data) => res.json(data))
+
+  res.json(message)
 }
 module.exports = postMessage
 // app.route('/api/threads/general').post((app) => {
