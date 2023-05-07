0daadc7699e009fc3b7e6907e93cb8fdf5aa74be
user <user@gmail.com>
2023-05-07 11:50:03 +0300

    Fix redirect with 303 status code

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,6 +1,6 @@
 const Message = require('../models/message.js')
 
-// post: /api/threads/general text=test; delete_password=aaaa
+// post: /api/threads/:board text=test; delete_password=aaaa
 // Response: // [{"_id":"6456b218ad743174db9b6dd0","text":"testXXX","created_on":"2023-05-06T20:01:28.805Z","bumped_on":"2023-05-06T20:01:28.805Z","replies":[],"replycount":0}]
 const postMessage = async (req, res) => {
   const message = await Message.create({
@@ -11,8 +11,9 @@ const postMessage = async (req, res) => {
     replies: [],
     replycount: 0,
   })
-
-  res.json(message)
+  // res.json(message)
+  // console.log(req.body.board)
+  res.redirect(303, `/b/${req.body.board}/`) // 303 parameter to make redirect work
 }
 module.exports = postMessage
 // app.route('/api/threads/general').post((app) => {
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const postMessage = require('../controllers/messageController.js')
 
 module.exports = async function (app) {
-  app.route('/api/threads/general').post(postMessage)
+  app.route('/api/threads/:board').post(postMessage)
+  // app.route('/api/threads/:board').get(postMessage)
 
   app.route('/api/replies/:board')
 }
